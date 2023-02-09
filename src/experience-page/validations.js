29d19354d69351ceaed3061872791177a6e5b1c3
Papika0 <72442767+Papika0@users.noreply.github.com>
2023-02-09 23:30:42 +0400

    experience page form html and css done

--- src/experience-page/validations.js
+++ src/experience-page/validations.js
@@ -1,7 +1,12 @@
 import { createHTML } from "../components/cvHtmlCreate.js";
 import { getAllOutputs } from "../components/localStorage.js";
+const backBtn = document.getElementById("back-btn");
 
 window.addEventListener("load", function () {
   createHTML();
   getAllOutputs();
+});
+
+backBtn.addEventListener("click", function () {
+  window.location.href = "../personal-page/personal.html";
 });
